cbe2e6e61c56a2310a760b89c6e31716aecf4c45
ansNaveed <66305771+ansNaveed@users.noreply.github.com>
2021-01-04 13:01:14 +0500

    Delete task7.js

--- task7.js
+++
@@ -1,5 +0,0 @@
-document.write("Variable names can only contain number,$ or ------"+
-                "For example $my_1stVariable"
-                   +"<br>" +"Variables must begin with a letter $ or_____. For example $name, _name or name"
-                   +"<br>" + "Variable names are case sensitive"
-                   +"<br>" +"Variable names should not be JS keyword)")
